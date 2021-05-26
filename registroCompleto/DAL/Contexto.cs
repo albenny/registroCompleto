6a06c64e7d9f12d52a639741383f29948b84a648
Albenny <Albenny@DESKTOP-D1GN2TO>
2021-05-26 14:26:21 -0400

    Agregar archivos de proyecto.

---
+++ registroCompleto/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using registroCompleto.Models;
+
+namespace registroCompleto.DAL
+
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Prestamos.db");
+        }
+    }
+}
